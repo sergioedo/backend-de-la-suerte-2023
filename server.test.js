2f72b10147a6ef97e95822437798a4a479e9735e
sergioedo <sergio.edo@gmail.com>
2023-03-11 15:45:55 +0100

    test - get created order and check createdAt date

--- server.test.js
+++ server.test.js
@@ -1,10 +1,27 @@
 const request = require('supertest')
-const app = require('./server')()
+const app = require('./server')
 
 test('Create an order with POST to /menu/order', async () => {
-    const response = await request(app)
+    const response = await request(app())
         .post('/menu/order')
         .expect('Content-Type', /json/)
         .expect(201)
     expect(response.body.createdAt).toBeDefined()
+})
+
+test('Query order and check createdAt', async () => {
+    const appInstance = app()
+    const createResponse = await request(appInstance)
+        .post('/menu/order')
+        .expect('Content-Type', /json/)
+        .expect(201)
+    expect(createResponse.body.id).toBeDefined()
+    expect(createResponse.body.createdAt).toBeDefined()
+    const { id, createdAt } = createResponse.body
+
+    const response = await request(appInstance)
+        .get(`/menu/order/${id}`)
+        .expect('Content-Type', /json/)
+        .expect(201)
+    expect(response.body.createdAt).toBe(createdAt)
 })
