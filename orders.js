7438d47ee8bef71c4e694cea2e35833f5a167940
sergioedo <sergio.edo@gmail.com>
2023-03-20 00:49:48 +0100

    refactor orders methods

--- orders.js
+++ orders.js
@@ -1,9 +1,27 @@
 const emojiBackend = require('./backend')
 
+const orderDishesElementToJSON = orderDishElement => {
+    return {
+        name: orderDishElement.get('🍽'),
+        quantity: Number(orderDishElement.get('🔢'))
+    }
+}
+
 module.exports = () => {
     const db = emojiBackend.createEmojiDB('👨‍🍳', true)
     const orderEntity = db.createEntity('📑', ['🆔', '🕓', '🪑'])
     const orderDishesEntity = db.createEntity('🗒', ['📑', '🍽', '🔢'])
+
+    const getOrderByElement = (orderElement) => {
+        const orderId = orderElement.get('🆔')
+        const orderDishes = orderDishesEntity.getElementsByField('📑', orderId).map(orderDishesElementToJSON)
+        return {
+            id: orderId,
+            table: Number(orderElement.get('🪑')),
+            dishes: orderDishes,
+            createdAt: orderElement.get('🕓')
+        }
+    }
 
     return {
         createOrder: (table, dishes) => {
@@ -12,60 +30,23 @@ module.exports = () => {
                 .set('🆔', orderId)
                 .set('🪑', table)
                 .set('🕓', Date.now())
-            const orderDishes = dishes.map(({ name, quantity }) => {
+            dishes.map(({ name, quantity }) => {
                 return orderDishesEntity.createElement()
                     .set('📑', orderId)
                     .set('🍽', name)
                     .set('🔢', quantity)
-            }).map(orderElement => {
-                return {
-                    name: orderElement.get('🍽'),
-                    quantity: Number(orderElement.get('🔢'))
-                }
             })
-            return {
-                id: orderElement.get('🆔'),
-                table: Number(orderElement.get('🪑')),
-                dishes: orderDishes,
-                createdAt: orderElement.get('🕓')
-            }
+            return getOrderByElement(orderElement)
         },
         getOrder: (orderId) => {
             const order = orderEntity.getElementsByField('🆔', orderId)
-            const orderDishes = orderDishesEntity.getElementsByField('📑', orderId).map(orderElement => {
-                return {
-                    name: orderElement.get('🍽'),
-                    quantity: Number(orderElement.get('🔢'))
-                }
-            })
             if (order.length === 0) {
                 throw Error(`order ${orderId} not found`)
             }
-            return {
-                id: order[0].get('🆔'),
-                table: Number(order[0].get('🪑')),
-                dishes: orderDishes,
-                createdAt: order[0].get('🕓')
-            }
+            return getOrderByElement(order[0])
         },
         getOrders: () => {
-            const responseOrders = []
-            orderEntity.getElements().map(order => {
-                const orderId = order.get('🆔')
-                const orderDishes = orderDishesEntity.getElementsByField('📑', orderId).map(orderElement => {
-                    return {
-                        name: orderElement.get('🍽'),
-                        quantity: Number(orderElement.get('🔢'))
-                    }
-                })
-                responseOrders.push({
-                    id: orderId,
-                    table: Number(order.get('🪑')),
-                    dishes: orderDishes,
-                    createdAt: order.get('🕓')
-                })
-            })
-            return responseOrders
+            return orderEntity.getElements().map(getOrderByElement)
         }
     }
 }
